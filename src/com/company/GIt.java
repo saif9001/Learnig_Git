c570b6d947784bd690a10a49d45b9a820fb2b219
Saif Sultan <saif.sultan@197NODMB25470.local>
2020-03-23 14:54:48 +0530

    Learnig_gitHub

---
+++ src/com/company/GIt.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class GIt {
+
+    public static void  main (String[] args){
+
+        System.out.println("Hello_Java");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Learnign Git");
-        System.out.println("Learnign Git");
+        System.out.println("Hello_Java");
     }
 }
